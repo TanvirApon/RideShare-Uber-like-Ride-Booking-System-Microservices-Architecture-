3655e080be0161a96970fb720aef3f43c3280c5a
tanvirhossainapon <tanvirhossainapon@gmail.com>
2026-09-10 21:16:18 +0600

    Feat: Ride model and dto added

---
+++ ride-service/src/main/java/com/rideshare/ride_service/model/RideStatus.java
@@ -0,0 +1,11 @@
+package com.rideshare.ride_service.model;
+
+public enum RideStatus {
+    REQUESTED,
+    MATCHING,
+    ACCEPTED,
+    DRIVER_ARRIVING,
+    RIDE_STARTED,
+    COMPLETED,
+    CANCELLED
+}
