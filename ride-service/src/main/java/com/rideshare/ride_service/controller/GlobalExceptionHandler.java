7afb7b5f18d567e8019245cef06e16e761193457
tanvirhossainapon <tanvirhossainapon@gmail.com>
2026-09-11 14:33:21 +0600

    chore: kafka config added

--- ride-service/src/main/java/com/rideshare/ride_service/controller/GlobalExceptionHandler.java
+++ ride-service/src/main/java/com/rideshare/ride_service/controller/GlobalExceptionHandler.java
@@ -1,4 +1,38 @@
 package com.rideshare.ride_service.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+   public ResponseEntity<Map<String, String>>
+   handleValidationError(MethodArgumentNotValidException ex) {
+
+       Map<String, String> errors = new HashMap<>();
+       ex.getBindingResult().getFieldErrors().forEach((fieldError) -> {
+           errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+       });
+       return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+   }
+
+   @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>>
+   handleRuntimeException(RuntimeException ex) {
+       log.error("Runtime Exception: {}", ex.getMessage());
+
+       Map<String, String> errors = new HashMap<>();
+       errors.put("error", ex.getMessage());
+       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+   }
 }
